83c4a7cc1dcef963da1da6a44eb912cad1b3be40
muhammadshaharyaraulakh <muhammadshaharyaraulakh@gmail.com>
2026-09-10 19:23:06 +0500

    Force HTTPS scheme for Vite assets and trust reverse proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production') || isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || str_contains(request()->url(), 'railway.app')) {
+            URL::forceScheme('https');
+        }
     }
 }
+
